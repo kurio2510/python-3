926c58f97eb4970b2be5cffe31af351e2f1ad155
kurio2510 <megannivip200345@gmail.com>
2026-05-17 16:31:57 +0700

    Updated

--- (2).py
+++ (2).py
@@ -1,9 +1,21 @@
-a = 10
-b = 5.5
-c = "Python"
-d = True
-
-print(a, type(a))
-print(b, type(b))
-print(c, type(c))
-print(d, type(d))
+print("Nhập vào 10 số nguyên khác nhau:")
+mang = []
+for i in range(10):
+    while True:
+        try:
+            so = int(input(f"Nhập phần tử thứ {i+1}: "))
+            if so in mang:
+                print("Số này đã tồn tại trong mảng. Vui lòng nhập số khác!")
+                continue
+            mang.append(so)
+            break
+        except ValueError:
+            print("Vui lòng chỉ nhập số nguyên hợp lệ!")
+print("\n[Kết quả hiển thị]")
+if len(mang) == 0:
+    print("Không có số lớn nhất")
+else:
+    so_lon_nhat = max(mang)
+    vi_tri = mang.index(so_lon_nhat)
+    print(f"Số lớn nhất: {so_lon_nhat}")
+    print(f"Vị trí: {vi_tri}")
