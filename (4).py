60efb5f2cd4c1ba23d70990b9a900984a1138774
kurio2510 <megannivip200345@gmail.com>
2026-05-17 16:33:33 +0700

    Updated

--- (4).py
+++ (4).py
@@ -1,5 +1,18 @@
-number_str = input("Nhập một chuỗi số thập phân: ")
-
-number_float = float(number_str)
-
-print("Số thực sau khi chuyển đổi là:", number_float)
+try:
+    n = int(input())
+    if n <= 0:
+        print("Không có ký tự số")
+    else:
+        characters = []
+        while len(characters) < n:
+            user_input = input()
+            parsed_chars = [x.strip("'").strip('"') for x in user_input.replace('[', '').replace(']', '').replace(',', ' ').split()]
+            characters.extend(parsed_chars)
+        characters = characters[:n]
+        digit_characters = [x for x in characters if x.isdigit()]
+        if digit_characters:
+            print(" ".join(digit_characters))
+        else:
+            print("Không có ký tự số")
+except ValueError:
+    print("Không có ký tự số")
